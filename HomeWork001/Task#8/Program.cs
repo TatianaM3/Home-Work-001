a7ffc603913d029d826b23449da76ccb648f5cea
Татьяна <yuki-shuichi@mail.ru>
2022-08-19 14:26:01 +0300

    Task#8

---
+++ HomeWork001/Task#8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int x = 1;
+
+while (x<=N)
+{
+    if (x % 2 == 0)
+    {
+        Console.WriteLine (x);
+    }
+    x = x + 1;
+}
