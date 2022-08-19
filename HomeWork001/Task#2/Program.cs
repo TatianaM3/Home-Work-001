8257e0fe290c412fc6a4ccefa81198f801702661
Татьяна <yuki-shuichi@mail.ru>
2022-08-19 13:04:20 +0300

    Task#2

---
+++ HomeWork001/Task#2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Input the fisrt number: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Input the second number: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine("max = ");
+    Console.WriteLine(numberA);
+}
+else
+{
+    Console.WriteLine("max = ");
+    Console.WriteLine(numberB);
+}
