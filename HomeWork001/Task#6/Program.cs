44f2a3ebb64d4a4b19d3bc41caa74c1544be265c
Татьяна <yuki-shuichi@mail.ru>
2022-08-19 14:07:00 +0300

    Task#6

---
+++ HomeWork001/Task#6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+double numberA = double.Parse(Console.ReadLine());
+
+if (numberA % 2 == 0)
+{
+    Console.WriteLine("Число является четным!");
+}
+else
+{
+    Console.WriteLine("Число является нечетным!"); 
+}
